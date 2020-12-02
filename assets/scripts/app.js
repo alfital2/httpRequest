a124fd9eedd4e1ba921a1bbd9f8420e10bb88fb1
talalfi <alfital2@gmail.com>
2020-12-02 14:50:21 +0200

    refactor - dividing logic into function + using promise

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,38 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
 
 const url='https://jsonplaceholder.typicode.com/posts';
 
-xhr.open('GET',url);
-xhr.send();
 
-xhr.responseType='json';
+function sendHttpRequest(method,url){
+    const promise = new Promise((rseolve,reject)=>{
+        const xhr = new XMLHttpRequest();
 
-xhr.onload = function(){
-    const listOfPosts = xhr.response;
-    for(const post of listOfPosts){
-        const postEl = document.importNode(postTemplate.content,true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.bosy;
-        listElement.append(postEl);
+        xhr.open(method,url);
+        xhr.send();
+        xhr.responseType='json';
+        
+        xhr.onload = function(){
+            rseolve(xhr.response);
+            const listOfPosts = xhr.response;
+            // iterate over every json and append it to the list of elements
+           
+            }
+    })
+   return promise;
+}
+
+
+function fetchPosts(url){
+    sendHttpRequest('GET',url).then(listOfPosts=>{
+        for(const post of listOfPosts){
+            const postEl = document.importNode(postTemplate.content,true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            listElement.append(postEl);
     }
-};
+})
+}
 
+fetchPosts(url)
