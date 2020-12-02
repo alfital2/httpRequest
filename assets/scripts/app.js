bb846e0e83e69068f226def706e5374290c3fd81
talalfi <alfital2@gmail.com>
2020-12-02 04:00:12 +0200

    sending the request and adding the recieved elements to the body of the html file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,22 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 const url='https://jsonplaceholder.typicode.com/posts';
-xhr.open('GET',url);
 
+xhr.open('GET',url);
 xhr.send();
+
+xhr.responseType='json';
+
+xhr.onload = function(){
+    const listOfPosts = xhr.response;
+    for(const post of listOfPosts){
+        const postEl = document.importNode(postTemplate.content,true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.bosy;
+        listElement.append(postEl);
+    }
+};
+
