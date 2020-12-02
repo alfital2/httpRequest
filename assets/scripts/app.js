8dac75aaeaf98e1f8abda082ef2dd64cd6ef235c
talalfi <alfital2@gmail.com>
2020-12-02 18:18:04 +0200

    adding functionality to the add button - sending a post request to the server

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,23 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
-
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
 
 const url='https://jsonplaceholder.typicode.com/posts';
 
 
-function sendHttpRequest(method,url){
+function sendHttpRequest(method,url,data){ // data is optional
     const promise = new Promise((rseolve,reject)=>{
         const xhr = new XMLHttpRequest();
 
         xhr.open(method,url);
-        xhr.send();
+
+        xhr.send(JSON.stringify(data));
+
         xhr.responseType='json';
         
         xhr.onload = function(){
             rseolve(xhr.response);
-            const listOfPosts = xhr.response;
-            // iterate over every json and append it to the list of elements
-           
             }
     })
    return promise;
@@ -25,26 +25,35 @@ function sendHttpRequest(method,url){
 
 
 async function fetchPosts(url){
-
-    const listOfPosts = await sendHttpRequest('GET',url);
-
+    const responseData = await sendHttpRequest(
+        'GET',
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const listOfPosts = responseData;
     for(const post of listOfPosts){
         const postEl = document.importNode(postTemplate.content,true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body;
         listElement.append(postEl);
-}
+    }
 }
 
-// function fetchPosts(url){
-//     sendHttpRequest('GET',url).then(listOfPosts=>{
-//         for(const post of listOfPosts){
-//             const postEl = document.importNode(postTemplate.content,true);
-//             postEl.querySelector('h2').textContent = post.title.toUpperCase();
-//             postEl.querySelector('p').textContent = post.body;
-//             listElement.append(postEl);
-//     }
-// })
-// }
+async function createPost(title,content){
+    const userId = Math.random();
+    const post= {
+        userId: userId,
+        body: content,
+        title: title
+    }
 
-fetchPosts(url)
+    sendHttpRequest('POST',url, post)
+}
+
+fetchButton.addEventListener('click',fetchPosts);
+form.addEventListener('submit',event=> {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector('#title').value;
+    const enteredContent = event.currentTarget.querySelector('#content').value;
+    createPost(enteredTitle,enteredContent);
+})
+
